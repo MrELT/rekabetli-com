51621cdb70ea45519b952171f8b60358f1cfead6
MrELT <efeeltutan751@gmail.com>
2026-05-31 21:48:07 +0300

    feat(notal): düzeltme3

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ["pdf-parse"],
-  async rewrites() {
-    return {
-      beforeFiles: [{ source: "/", destination: "/index.html" }],
-    };
-  },
 };
 
 export default nextConfig;
